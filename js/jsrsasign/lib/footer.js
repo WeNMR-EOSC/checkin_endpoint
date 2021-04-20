bee0041df7f7faf9448caeed76f8c35475740263
Brian Jimenez Garcia <b.jimenezgarcia@uu.nl>
2021-04-20 13:23:27 +0200

    Updated to the last version jsrsasign lib

--- js/jsrsasign/lib/footer.js
+++ js/jsrsasign/lib/footer.js
@@ -8,14 +8,21 @@ exports.DSA = KJUR.crypto.DSA;
 exports.Signature = KJUR.crypto.Signature;
 exports.MessageDigest = KJUR.crypto.MessageDigest;
 exports.Mac = KJUR.crypto.Mac;
+exports.Cipher = KJUR.crypto.Cipher;
 exports.KEYUTIL = KEYUTIL;
 exports.ASN1HEX = ASN1HEX;
 exports.X509 = X509;
+exports.X509CRL = X509CRL;
 exports.CryptoJS = CryptoJS;
 
 // ext/base64.js
 exports.b64tohex = b64tohex;
 exports.b64toBA = b64toBA;
+
+// ext/ec*.js
+exports.ECFieldElementFp = ECFieldElementFp;
+exports.ECPointFp = ECPointFp;
+exports.ECCurveFp = ECCurveFp;
 
 // base64x.js
 exports.stoBA = stoBA;
@@ -30,7 +37,6 @@ exports.b64utob64 = b64utob64;
 exports.hex2b64 = hex2b64;
 exports.hextob64u = hextob64u;
 exports.b64utohex = b64utohex;
-exports.b64tohex = b64tohex;
 exports.utf8tob64u = utf8tob64u;
 exports.b64utoutf8 = b64utoutf8;
 exports.utf8tob64 = utf8tob64;
@@ -39,20 +45,41 @@ exports.utf8tohex = utf8tohex;
 exports.hextoutf8 = hextoutf8;
 exports.hextorstr = hextorstr;
 exports.rstrtohex = rstrtohex;
+exports.hextob64 = hextob64;
+exports.hextob64nl = hextob64nl;
+exports.b64nltohex = b64nltohex;
+exports.hextopem = hextopem;
+exports.pemtohex = pemtohex;
+exports.hextoArrayBuffer = hextoArrayBuffer;
+exports.ArrayBuffertohex = ArrayBuffertohex;
+exports.zulutomsec = zulutomsec;
+exports.zulutosec = zulutosec;
+exports.zulutodate = zulutodate;
+exports.datetozulu = datetozulu;
+exports.uricmptohex = uricmptohex;
+exports.hextouricmp = hextouricmp;
+exports.ipv6tohex = ipv6tohex;
+exports.hextoipv6 = hextoipv6;
+exports.hextoip = hextoip;
+exports.iptohex = iptohex;
+exports.ucs2hextoutf8 = ucs2hextoutf8;
+exports.encodeURIComponentAll = encodeURIComponentAll;
 exports.newline_toUnix = newline_toUnix;
 exports.newline_toDos = newline_toDos;
+exports.hextoposhex = hextoposhex;
 exports.intarystrtohex = intarystrtohex;
 exports.strdiffidx = strdiffidx;
+exports.oidtohex = oidtohex;
+exports.hextooid = hextooid;
+exports.strpad = strpad;
+exports.bitstrtoint = bitstrtoint;
+exports.inttobitstr = inttobitstr;
 
+// name spaces
 exports.KJUR = KJUR;
 exports.crypto = KJUR.crypto;
 exports.asn1 = KJUR.asn1;
 exports.jws = KJUR.jws;
 exports.lang = KJUR.lang;
 
-exports.readFileUTF8 = readFileUTF8;
-exports.readFileHexByBin = readFileHexByBin;
-exports.readFile = readFile;
-exports.saveFile = saveFile;
-exports.saveFileBinByHex = saveFileBinByHex;
 
